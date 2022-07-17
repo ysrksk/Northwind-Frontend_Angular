5d405991f236941867fb9cd54bcf03b592790bc1
Yaşar Küskü <yasarkusku@hotmail.com>
2022-07-17 19:46:29 +0300

    2.Ders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { CategoryComponent } from './component/category/category.component';
+import { NaviComponent } from './component/navi/navi.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../models/productResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient:HttpClient) { }
+  apiUrl = "https://localhost:44398/api/products/getall"
+
+  getProducts():Observable<ProductResponseModel>{
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+  };
+}
+
